074ddfb35de297a68cd40984da4737793492f8f4
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2011-03-18 15:48:31 +0100

    Added Character.java

---
+++ src/no/ntnu/stud/flatcraft/entities/Character.java
@@ -0,0 +1,59 @@
+package no.ntnu.stud.flatcraft.entities;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Character extends GameEntity {
+	public void reset(){
+		
+	}
+	
+	public void update(GameContainer container, StateBasedGame game, int delta){
+		
+	}
+	
+	public void setWeapon(weaponType weapon){
+		
+	}
+	
+	public void fireWeapon(){
+		
+	}
+	
+	public void die(){
+		
+	}
+	
+	public void takeDamage(Vector2f force, int fromPlayer, weaponType weapon){
+		
+	}
+	
+	public void spawn(Vector2f position){
+		
+	}
+	
+	public int increaseHealth(int amount){
+		return 0; //returns health after increase;
+	}
+	
+	public int increaseArmor(int amount){
+		return 0; //returns armor after increase;
+	}
+	
+	public void giveWeapon(weaponType weapon, int ammo){
+		
+	}
+	
+	public boolean isAlive(){
+		return alive;
+	}
+	
+	Player player; //handle to the player that controls this character.
+	boolean alive;
+	float reloadTimer;
+	int damageTaken;
+	int ammo;
+	int health;
+	int armor;
+}
